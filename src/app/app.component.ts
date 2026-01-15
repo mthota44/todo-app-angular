9047e211b848727ac7a248e2138c8f41a06721c8
mthota44 <manojkumarthota44@gmail.com>
2026-01-15 11:55:58 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormControl } from '@angular/forms';
+import { NgIf, NgFor, NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, NgIf, NgFor, NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'todo-app';
+
+  todoControl = new FormControl('');
+  todos = [
+    { id: 1, title: 'Learn Angular Basics', completed: false },
+    { id: 2, title: 'Understand Directives', completed: true }
+  ];
+
+  addTodo() {
+    const value = this.todoControl.value;
+
+    if (!value || value.trim().length < 3) return;
+
+    this.todos.push({
+      id: Date.now(),
+      title: value,
+      completed: false
+    });
+
+    this.todoControl.reset();
+  }
+
+  toggleTodo(todo: any) {
+    todo.completed = !todo.completed;
+  }
+
+  deleteTodo(id: number) {
+    this.todos = this.todos.filter(todo => todo.id !== id);
+  }
 }
