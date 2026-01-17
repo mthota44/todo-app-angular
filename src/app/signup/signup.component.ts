a2fe8315bf03faf6425f5a013fbc3fcae597b760
mthota44 <manojkumarthota44@gmail.com>
2026-01-17 23:51:24 +0530

    having bug

---
+++ src/app/signup/signup.component.ts
@@ -0,0 +1,57 @@
+import { Component,inject } from '@angular/core';
+import {ReactiveFormsModule,FormGroup,FormControl,FormBuilder,AbstractControl,ValidationErrors,Validators} from '@angular/forms';
+import { NgIf, NgFor, NgClass,CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-signup',
+  standalone: true,
+  imports: [],
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.css'
+})
+export class SignupComponent {
+
+  // registration form
+  private fb = inject(FormBuilder);
+  userForm = this.fb.group({
+
+    userInfo: this.fb.group({
+      username: ['', Validators.required],
+      age: [null, Validators.required],
+      gender: ['', Validators.required],
+      mobile: [
+        '',
+        [
+          Validators.required,
+          this.mobileNumberValidator
+        ]
+      ]
+    }),
+
+    address: this.fb.group({
+      street: ['', Validators.required],
+      city: ['', Validators.required],
+      pincode: ['', Validators.required]
+    })
+  });
+
+  mobileNumberValidator(control: AbstractControl): ValidationErrors | null {
+    const value = control.value;
+
+    if (!value) {
+      return null; // required validator will handle empty case
+    }
+
+    const mobileRegex = /^[6-9]\d{9}$/;
+
+    return mobileRegex.test(value)
+      ? null
+      : { invalidMobile: true };
+
+  }
+
+  submitUserForm() {
+    console.log('Complete FormGroup:', this.userForm);
+    console.log('Form Value:', this.userForm.value);
+  }
+}
